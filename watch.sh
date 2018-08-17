cf3a88d4de98754baf3b10ef91a380b5c917f1a9
hosamhany <7ossHany@gmail.com>
2018-08-17 15:54:36 +0200

    added the interval option to add the interval of time in seconds to watch the command

--- watch.sh
+++ watch.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+infinite_watch (){
+	while :; do
+		clear;
+		$@;
+		sleep 2;
+	done
+}
 
-commandArray=( "$@" );
-COMMANDS="${commandArray[*]}"
-while :; do
-	clear;
-	$COMMANDS;
-	sleep 2;
-done
+watch_with_interval(){
+	interval=$1
+	shift
+	rest=$@
+	while [ $SECONDS -lt $interval ]; do
+		clear;
+		 $rest
+		sleep 2;
+	done
+
+}
